a544be56e3351438e73f760b9429ec753b5ec3b9
AdminFilipeOliveira <126712425+AdminFilipeOliveira@users.noreply.github.com>
2023-06-02 20:32:49 +0100

    ComprasLivraria

--- src/ComprasLivraria.java
+++ src/ComprasLivraria.java
@@ -1,15 +1,21 @@
 /*
  * Desafio
-Crie um programa em Java para calcular o valor total de uma compra de livros em uma livraria. O programa deve perguntar o nome, o preço e a quantidade de cada livro que o cliente deseja comprar. O programa deve exibir o valor total da compra e o número de livros comprados.
+Crie um programa em Java para calcular o valor total de uma compra de livros em uma livraria. 
+O programa deve perguntar o nome, o preço e a quantidade de cada livro que o cliente deseja comprar. 
+O programa deve exibir o valor total da compra e o número de livros comprados.
 
 Entrada
-A entrada consiste em dados informados pelo usuário via teclado. O usuário deve informar o nome do livro (uma string sem espaços), o preço do livro (um número real) e a quantidade de livros desejados (um número inteiro).
+A entrada consiste em dados informados pelo usuário via teclado. 
+O usuário deve informar o nome do livro (uma string sem espaços), o preço do livro (um número real) e a quantidade de livros desejados (um número inteiro).
 
 Saída
-A saída consiste em mensagens exibidas na tela do usuário. O programa deve exibir o valor total da compra, formatado com duas casas decimais, seguido do número de livros comprados. Por fim, o programa deve exibir a mensagem "Obrigado por comprar na nossa livraria!"
+A saída consiste em mensagens exibidas na tela do usuário. 
+O programa deve exibir o valor total da compra, formatado com duas casas decimais,
+ seguido do número de livros comprados. Por fim, o programa deve exibir a mensagem "Obrigado por comprar na nossa livraria!"
 
 Exemplos
-A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. 
+Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis
 
 Entrada	Saída
 Senhor dos Anéis - As Duas Torres
